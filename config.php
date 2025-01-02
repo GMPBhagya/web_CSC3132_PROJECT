d054357dbf06cb53240dfa7ef575a9b3e53faa23
GMPBhagya <139644464+GMPBhagya@users.noreply.github.com>
2025-01-02 10:26:09 +0000

    config.php file created

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root"; // your MySQL username
-$password = ""; // your MySQL password
-$dbname = "bookhevan"; // the database name
+$servername = "localhost"; // Your database server (usually localhost)
+$username = "root"; // Your database username
+$password = ""; // Your database password
+$dbname = "BookHevan"; // The database name
 
-// Create connection
+// Create a connection to the database
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check if the connection is successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
